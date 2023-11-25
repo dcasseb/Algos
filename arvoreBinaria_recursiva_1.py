438fc352ce52d34b1c476769a23cffaa333178ea
dcasseb <77282586+dcasseb@users.noreply.github.com>
2023-11-25 13:50:44 -0300

    Update arvoreBinaria_recursiva.py
    Adicionado as ordenações de maneira recursiva.

--- arvoreBinaria_recursiva_1.py
+++ arvoreBinaria_recursiva_1.py
@@ -35,7 +35,7 @@ class ArvoreBinaria:
             self.root = Node(value)
         self.__insere_recursivo(self.root, value)
 
-    def BFS(self):
+    def BuscaEmLargura(self):
         current_node = self.root
         queue = []
         resultados = []
@@ -50,6 +50,24 @@ class ArvoreBinaria:
                 queue.append(current_node.right)
         return resultados
     
+    #implementar BuscaEmLargura de modo que a saida seja do ultimo nivel para o primeiro
+    #no loop, mandar para a lista 'resultados' os nós da direita pra esquerda, ao invés da esquerda para direita, e imprimir a lista de trás pra frente
+
+    def BuscaEmLargura_contrario(self):
+        current_node = self.root
+        queue = []
+        resultados = []
+        queue.append(current_node)
+
+        while(len(queue)) > 0:
+            current_node = queue.pop(0)
+            resultados.append(current_node.value)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+        return resultados[::-1] #inverte a lista
+    
     def pre_ordem(self):
         resultados = []
         def caminho(current_node):
@@ -60,6 +78,14 @@ class ArvoreBinaria:
                 caminho(current_node.right)
         caminho(self.root)
         return resultados
+    
+    def preOrdemRecursivo(self):
+        self.visitaRecursiva(self.root)
+    def visitaRecursiva(self, root):
+        if root:
+            print(root.value)
+            self.visitaRecursiva(root.left)
+            self.visitaRecursiva(root.right)
     
     def in_ordem(self):
         resultados = []
@@ -72,6 +98,14 @@ class ArvoreBinaria:
         caminho(self.root)
         return resultados
     
+    def inOrdemRecursivo(self):
+        self.visitaRecursiva(self.root)
+    def visitaRecursiva(self, root):
+        if root:
+            self.visitaRecursiva(root.left)
+            print(root.value)
+            self.visitaRecursiva(root.right)
+
     def pos_ordem(self):
         resultados = []
         def caminho(current_node):
@@ -82,20 +116,36 @@ class ArvoreBinaria:
             resultados.append(current_node.value)
         caminho(self.root)
         return resultados
+    
+    def posOrdemRecursivo(self):
+        self.visitaRecursiva(self.root)
+    def visitaRecursiva(self, root):
+        if root:
+            self.visitaRecursiva(root.left)
+            self.visitaRecursiva(root.right)
+            print(root.value)
+    
+    #Implemetar método para calcular a altura de uma árvore, de uma sub-árvore da esquerda e sub-árvore da direita e qual sub-árvore é maior
+    
+    #ESTUDAR VARIAÇÕES DOS MÉTODOS DE ORDENAÇÃO PARA A PROVA
 
-#minha_arvore = ArvoreBinaria()
-#minha_arvore.insere(4)
-#minha_arvore.insere(2)
-#minha_arvore.insere(1)
-#minha_arvore.insere(3)
-#minha_arvore.insere(6)
-#minha_arvore.insere(7)
-#minha_arvore.insere(5)
+minha_arvore = ArvoreBinaria()
+minha_arvore.insere(4)
+minha_arvore.insere(2)
+minha_arvore.insere(1)
+minha_arvore.insere(3)
+minha_arvore.insere(6)
+minha_arvore.insere(7)
+minha_arvore.insere(5)
 
-#print('Raiz:', minha_arvore.root.value)
-#print('Raiz - esquerda:', minha_arvore.root.left.value)
-#print('Raiz - direita:', minha_arvore.root.right.value)
-#print(minha_arvore.BFS())
-#print(minha_arvore.pre_ordem())
-#print(minha_arvore.in_ordem())
-#print(minha_arvore.pos_ordem())
+print('Raiz:', minha_arvore.root.value)
+print('Raiz - esquerda:', minha_arvore.root.left.value)
+print('Raiz - direita:', minha_arvore.root.right.value)
+print(minha_arvore.BuscaEmLargura())
+print(minha_arvore.BuscaEmLargura_contrario())
+print(minha_arvore.pre_ordem())
+print(minha_arvore.in_ordem())
+print(minha_arvore.pos_ordem())
+minha_arvore.preOrdemRecursivo()
+minha_arvore.inOrdemRecursivo()
+minha_arvore.posOrdemRecursivo()
